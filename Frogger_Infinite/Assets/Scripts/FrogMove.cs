c96cb9a6334b92d02af56a981d95f61fb123a9e2
holgidonath <hdonath@itba.edu.ar>
2021-09-10 00:10:22 -0300

    movimiento frog

--- Frogger_Infinite/Assets/Scripts/FrogMove.cs
+++ Frogger_Infinite/Assets/Scripts/FrogMove.cs
@@ -7,6 +7,8 @@ public class FrogMove : MonoBehaviour
     public float speed = 1f;
     private Vector2 _movement;
     private Animator _anmCtrl;
+    private bool running;
+    private int lookingHorizontal;
    
     // Start is called before the first frame update
     void Start()
@@ -26,31 +28,49 @@ public class FrogMove : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             _movement.x = speed;
-            _anmCtrl.SetBool("run",true);
-
+            running = true;
+            lookingHorizontal = 1;
+            _anmCtrl.SetInteger ("lookDir", 0);
+            _anmCtrl.SetBool ("run", true);
+        
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             _movement.x = -speed;
-            _anmCtrl.SetBool("run",true);
+            running = true;
+            lookingHorizontal = 1;
+            _anmCtrl.SetInteger ("lookDir", 1);
+            _anmCtrl.SetBool ("run", true);
+           
         }
         else if  (Input.GetKey(KeyCode.UpArrow))
         {
             _movement.y = speed;
-            _anmCtrl.SetBool("run",true);
+            running = true;
+            lookingHorizontal = 0;
+            _anmCtrl.SetInteger ("lookDir", 3);
+            _anmCtrl.SetBool ("run", true);
+            
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
             _movement.y = -speed;
-            _anmCtrl.SetBool("run",true);
+            running = true;
+            lookingHorizontal = 0;
+            _anmCtrl.SetInteger ("lookDir", 2);
+            _anmCtrl.SetBool ("run", true);
+    
+            
         }
         else
         {
             _movement.x = 0f;
             _movement.y = 0f;
+            running = false;
+            _anmCtrl.SetBool ("run", false);
         }
 
-        _anmCtrl.SetBool("run", Mathf.Abs(_movement.x) > 0.001f ||  Mathf.Abs(_movement.y) > 0.001f);
+       
 
     }
 
@@ -58,11 +78,19 @@ public class FrogMove : MonoBehaviour
     {
         var dt = Time.deltaTime;
         var curPos = transform.position;
-        
-        float newposx = curPos.x + _movement.x * dt;
-        float newposy = curPos.y + _movement.y * dt;
-
-        transform.position = new Vector3(newposx,newposy,curPos.z);
+        if (running)
+        {
+            if(lookingHorizontal == 1)
+            {
+                float newposx = (curPos.x + (_movement.x * dt)/10f);
+                transform.position = new Vector3(newposx,curPos.y,curPos.z);
+            }
+            else if (lookingHorizontal == 0)
+            {
+            float newposy = (curPos.y + (_movement.y * dt)/10f);
+            transform.position = new Vector3(curPos.x,newposy,curPos.z);
+            }
+        }
     }
 
 
