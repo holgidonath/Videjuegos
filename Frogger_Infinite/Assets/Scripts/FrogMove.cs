65e208f79f2755c9e4976cd1f0dfc92a2aea4e77
rodrifera <rfera@itba.edu.ar>
2021-09-11 20:05:59 -0300

    arranco enemy

--- Frogger_Infinite/Assets/Scripts/FrogMove.cs
+++ Frogger_Infinite/Assets/Scripts/FrogMove.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class FrogMove : MonoBehaviour
 {
-    public float speed = 1f;
-    private Vector2 _movement;
+    public float speed = 50f;
+    public Vector2 _movement;
     private Animator _anmCtrl;
     private bool running;
     private int lookingHorizontal;
@@ -27,37 +27,37 @@ public class FrogMove : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            _anmCtrl.SetInteger("lookDir", 0);
             _movement.x = speed;
             running = true;
             lookingHorizontal = 1;
-            _anmCtrl.SetInteger ("lookDir", 0);
             _anmCtrl.SetBool ("run", true);
         
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
+            _anmCtrl.SetInteger("lookDir", 1);
             _movement.x = -speed;
             running = true;
             lookingHorizontal = 1;
-            _anmCtrl.SetInteger ("lookDir", 1);
             _anmCtrl.SetBool ("run", true);
            
         }
         else if  (Input.GetKey(KeyCode.UpArrow))
         {
+            _anmCtrl.SetInteger("lookDir", 3);
             _movement.y = speed;
             running = true;
             lookingHorizontal = 0;
-            _anmCtrl.SetInteger ("lookDir", 3);
             _anmCtrl.SetBool ("run", true);
             
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
+            _anmCtrl.SetInteger("lookDir", 2);
             _movement.y = -speed;
             running = true;
             lookingHorizontal = 0;
-            _anmCtrl.SetInteger ("lookDir", 2);
             _anmCtrl.SetBool ("run", true);
     
             
@@ -82,14 +82,23 @@ public class FrogMove : MonoBehaviour
         {
             if(lookingHorizontal == 1)
             {
-                float newposx = (curPos.x + (_movement.x * dt)/15f);
+                float newposx = (curPos.x + (_movement.x * dt));
                 transform.position = new Vector3(newposx,curPos.y,curPos.z);
             }
             else if (lookingHorizontal == 0)
             {
-            float newposy = (curPos.y + (_movement.y * dt)/15f);
-            transform.position = new Vector3(curPos.x,newposy,curPos.z);
+                float newposy = (curPos.y + (_movement.y * dt));
+                transform.position = new Vector3(curPos.x,newposy,curPos.z);
             }
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            Debug.Log("aloja");
         }
     }
 
