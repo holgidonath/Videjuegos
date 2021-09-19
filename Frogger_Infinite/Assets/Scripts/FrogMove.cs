d0e399aecd17d00d23b8f9eba1583b414daef554
holgidonath <hdonath@itba.edu.ar>
2021-09-19 14:33:24 -0300

    menu working

--- Frogger_Infinite/Assets/Scripts/FrogMove.cs
+++ Frogger_Infinite/Assets/Scripts/FrogMove.cs
@@ -287,7 +287,8 @@ public class FrogMove : MonoBehaviour
         {
             Destroy(prefab);
         }*/
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("LossScreen");
+
     }
 
 
